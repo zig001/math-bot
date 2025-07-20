8192f1d1dd79122ce2410ff3d9aad7c6b17d0fa3
zig365_ <49755149+zig001@users.noreply.github.com>
2025-07-20 17:24:51 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,16 +4,13 @@ from database.crud import init_db
 from handlers import admin, collection, user
 
 def main():
-    # Инициализация бота и БД
     bot = TeleBot(BOT_TOKEN)
     init_db()
     
-    # Регистрация обработчиков
     admin.setup_admin_handlers(bot)
     collection.setup_collection_handlers(bot)
     user.setup_user_handlers(bot)
     
-    # Запуск бота
     print("Бот запущен...")
     bot.infinity_polling()
 
